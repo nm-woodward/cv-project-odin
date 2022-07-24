1b17e86929cce87ee8faf87607ee0de6e7243c50
nm-woodward <nowoodward15@gmail.com>
2022-07-23 17:34:57 -0700

    Created base HTML/CSS

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # CV Builder App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
 ## Goal
 
 Create a live-update resume that includes the following:
@@ -9,3 +7,5 @@ Create a live-update resume that includes the following:
 - A section to add general information like name, email, phone number.
 - A section to add your educational experience (school name, title of study, date of study)
 - A section to add practical experience (company name, position title, main tasks of your jobs, date from and until when you worked for that company)
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="aside">
+        <div className="sectionHeader">Skills</div>
+      </div>
+      <div id="main">
+        <div id="heading">
+          <div id="fullName">Noah Woodward</div>
+          <div id="title">Software Engineer</div>
+          <div id="profile">
+            <h2 id="profileHeader">Profile</h2>
+            <div id="profileBody">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia d
+              </p>
+            </div>
+          </div>
+        </div>
+        <div id="education">
+          <div className="sectionHeader">Education</div>
+        </div>
+        <div id="experience">
+          <div className="sectionHeader">Experience</div>
+        </div>
+      </div>
     </div>
   );
 }
